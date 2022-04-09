6c19334cb413c7b603b921a8d3af1698985b0965
syafiqsyauqi <91374062+syafiqsyauqi@users.noreply.github.com>
2022-04-09 18:38:53 +0700

    Add files via upload

---
+++ praktikum-minggu7.php/soal1.php
@@ -0,0 +1,6 @@
+<?php
+    $string = "D3 Rekayasa Perangkat Lunak Aplikasi";
+    echo "<br>". substr($string, 3);
+    echo "<br>". substr($string, -8);
+    echo "<br>". substr($string, 3, 24);
+?>
